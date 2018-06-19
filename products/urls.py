9ff1d7936f84915decd1deff5c82ccbaa230d850
e-landeros <landerosedgard@gmail.com>
2018-06-18 22:27:06 -0700

    home templates and forms connected
    needs to be polished

--- products/urls.py
+++ products/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('create', views.create, name='create'),
+    path('<int:product_id>', views.detail, name='datail'),
+    path('<int:product_id>/upvote', views.upvote, name='upvote'),
 ]
